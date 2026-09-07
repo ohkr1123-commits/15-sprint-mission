f16b26d90a6b1afb25f2df30689d8e0d1841d1a7
JU YEUNG <ohkr1123@gmail.com>
2026-09-07 18:08:09 +0900

    refactor: DiscodeitApplication의 Service 초기화를 Spring Context 기반으로 변경

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,13 +1,29 @@
 package com.sprint.mission.discodeit;
 
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiscodeitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscodeitApplication.class, args);
-	}
 
+		ConfigurableApplicationContext context =
+				SpringApplication.run(DiscodeitApplication.class, args);
+
+		UserService userService =
+				context.getBean(UserService.class);
+
+		ChannelService channelService =
+				context.getBean(ChannelService.class);
+
+		MessageService messageService =
+				context.getBean(MessageService.class);
+
+		System.out.println("Spring Bean 조회 완료");
+	}
 }
