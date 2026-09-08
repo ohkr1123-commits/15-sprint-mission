50069570da68be71cc94b74877c6ae47dab267c4
JU YEUNG <ohkr1123@gmail.com>
2026-09-08 16:17:18 +0900

    feat: UserStatus 클래스 및 온라인 상태 확인 기능 추가

---
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -0,0 +1,43 @@
+package com.sprint.mission.discodeit.entity;
+
+import lombok.Getter;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+public class UserStatus {
+
+    private UUID id;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private UUID userId;
+    private Instant lastActiveAt;
+
+    public UserStatus(UUID userId) {
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
+        this.updatedAt = this.createdAt;
+        this.userId = userId;
+        this.lastActiveAt = Instant.now();
+    }
+
+    public void updateLastActiveAt() {
+        this.lastActiveAt = Instant.now();
+    }
+
+    public boolean isOnline() {
+        if (this.lastActiveAt == null) {
+            return false;
+        }
+        Instant now = Instant.now();
+
+        // 현재 시간과 마지막 접속 시간 사이의 차이 계산
+        Duration duration = Duration.between(this.lastActiveAt, now);
+
+        // 시간 차이가 0분 이상이고 5분 이하인지 확인
+        return !duration.isNegative()
+                && duration.compareTo(Duration.ofMinutes(5)) <= 0;
+    }
+}
