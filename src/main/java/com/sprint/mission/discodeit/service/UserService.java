a5897237e0c45a734f6aeee44f36fafa40ddc6ce
JU YEUNG <ohkr1123@gmail.com>
2026-09-10 18:10:29 +0900

    docs: UserService 메서드별 기능 주석 추가

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -8,17 +8,22 @@ import java.util.UUID;
 
 public interface UserService {
 
+    //유저 생성기능
     User create(
             UserCreateRequest userRequest,
             BinaryContentCreateRequest profileRequest
     );
 
+    //유저의 id로 조회하는 기능
     User read(UUID id);
 
+    //등록된 모든 유저를 조회하는 기능
     List<User> readAll();
 
+    //등록된 유저의 정보를 수정하는 기능
     User update(UUID id, String name, String email, String password);
 
+    //등록된 유저의 id로 유저의 정보를 삭제하는 기능
     void delete(UUID id);
 
 }
