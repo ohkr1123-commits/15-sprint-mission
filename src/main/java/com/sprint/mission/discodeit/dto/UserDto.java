bc0733824f6e2af9df8c877449dc4c1c9523065e
JU YEUNG <ohkr1123@gmail.com>
2026-09-10 17:59:24 +0900

    feat: UserDto를 record로 생성

---
+++ src/main/java/com/sprint/mission/discodeit/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.sprint.mission.discodeit.dto;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record UserDto(
+        UUID id,
+        Instant createdAt,
+        Instant updatedAt,
+        String name,
+        String email,
+        UUID profileId,
+        boolean online
+) {
+}
