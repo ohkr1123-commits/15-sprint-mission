520b316e85dd3059e877167a6066b68b5256b644
JU YEUNG <ohkr1123@gmail.com>
2026-09-11 10:30:59 +0900

    refactor: UserUpdateRequest의 내용 수정

--- src/main/java/com/sprint/mission/discodeit/dto/User/UserUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/User/UserUpdateRequest.java
@@ -3,7 +3,7 @@ package com.sprint.mission.discodeit.dto.User;
 import java.util.UUID;
 
 public record UserUpdateRequest(
-        UUID id,
+
         String name,
         String email,
         String password
