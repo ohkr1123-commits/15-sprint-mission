7251174f2408da8f2fe3dbb19d5e15a58f1b6761
JU YEUNG <ohkr1123@gmail.com>
2026-09-21 11:34:50 +0900

    refactor: Auth API를 RESTful 구조로 변경

--- src/main/java/com/sprint/mission/discodeit/Controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/Controller/AuthController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,7 +20,7 @@ public class AuthController {
 
   private final AuthService authService;
 
-  @RequestMapping(path = "login")
+  @PostMapping("/login")
   public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest) {
     User user = authService.login(loginRequest);
     return ResponseEntity
